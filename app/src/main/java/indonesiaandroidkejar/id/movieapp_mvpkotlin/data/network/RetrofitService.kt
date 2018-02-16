a8acc6cff1acc48cf838e0a334626e1419c4201b
cakhandi95 <akang.handy95@gmail.com>
2018-02-17 06:10:26 +0700

    implementasi retrofit kotlin

---
+++ app/src/main/java/indonesiaandroidkejar/id/movieapp_mvpkotlin/data/network/RetrofitService.kt
@@ -0,0 +1,8 @@
+package indonesiaandroidkejar.id.movieapp_mvpkotlin.data.network
+
+/**
+ * Created by Handy on 17/02/18.
+ * akang.handy95@gmail.com
+ */
+class RetrofitService {
+}
---
+++ app/src/main/java/indonesiaandroidkejar/id/movieapp_mvpkotlin/utilities/Const.kt
@@ -0,0 +1,12 @@
+package indonesiaandroidkejar.id.movieapp_mvpkotlin.utilities
+
+/**
+ * Created by Handy on 17/02/18.
+ * akang.handy95@gmail.com
+ */
+class Const {
+
+    val TIMEOUT:Long = 30
+    val BASE_URL = ""
+
+}
